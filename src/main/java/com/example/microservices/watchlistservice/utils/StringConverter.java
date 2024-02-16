b094de047f80724e39e234c1254d4a9645dd0dd5
Florian Speicher <florian.speicher@abatplus.de>
2024-02-16 14:25:20 +0100

    Erster Prototype des StringConverter mit Hilfe eines ObjectMappers fertig!

--- src/main/java/com/example/microservices/watchlistservice/utils/StringConverter.java
+++ src/main/java/com/example/microservices/watchlistservice/utils/StringConverter.java
@@ -3,37 +3,93 @@ package com.example.microservices.watchlistservice.utils;
 import com.example.microservices.watchlistservice.dto.Actor;
 import com.example.microservices.watchlistservice.dto.Movie;
 import com.example.microservices.watchlistservice.dto.Regisseur;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.List;
 
 public abstract class StringConverter {
 
-    //TODO implement String Convertion
-    //TODO Json Mapper anschauen, ob dieser die Daten richtig in die PObjektform convertiert
-   public static Movie stringToMovie(String input){
-       return null;
+    static ObjectMapper om = new ObjectMapper();
+
+   public static Movie stringToMovie(String input) {
+       Movie movie;
+       try {
+           movie = om.readValue(input, Movie.class);
+       } catch (JsonProcessingException e) {
+           System.err.println("JsonProcessingException in method: stringToMovie");
+           return null;
+       }
+       return movie;
    }
 
    public static List<Actor> stringToActorList(String input){
-       return null;
+       List<Actor> actorList;
+       try {
+           actorList = om.readValue(input, new TypeReference<List<Actor>>() {});
+       } catch (JsonProcessingException e) {
+           System.err.println("JsonProcessingException in method: stringToActorList");
+           return null;
+       }
+       return actorList;
    }
 
    public static List<Regisseur> stringToRegisseurList(String input){
-       return null;
+       List<Regisseur> regisseurList;
+       try {
+           regisseurList = om.readValue(input, new TypeReference<List<Regisseur>>() {});
+       } catch (JsonProcessingException e) {
+           System.err.println("JsonProcessingException in method: stringToRegisseurList");
+           return null;
+       }
+       return regisseurList;
    }
 
    public static String stringToCommentString(String input){
-       return "null";
+       return "{\"comment\":\"" + input + "\"}";
    }
 
-   public static List<Movie> stringToMovieList(String input){return null;}
+   public static List<Movie> stringToMovieList(String input){
+       List<Movie> movieList;
+       try {
+           movieList = om.readValue(input, new TypeReference<List<Movie>>() {});
+       } catch (JsonProcessingException e) {
+           System.err.println("JsonProcessingException in method: stringToMovieList");
+           return null;
+       }
+       return movieList;
+   }
 
    public static String movieToString(Movie movie){
-       return "";
+       try {
+           return om.writeValueAsString(movie);
+       } catch (JsonProcessingException e) {
+           System.err.println("JsonProcessingException in method: movieToString");
+           return null;
+       }
    }
 
-   public static Regisseur stringToRegisseur(String input){return new Regisseur("asd", "sad");}
+   public static Regisseur stringToRegisseur(String input){
+       Regisseur regisseur;
+       try {
+           regisseur = om.readValue(input, Regisseur.class);
+       } catch (JsonProcessingException e) {
+           System.err.println("JsonProcessingException in method: stringToRegisseur");
+           return null;
+       }
+       return regisseur;
+   }
 
-    public static Actor stringToActor(String input){return new Actor("", "");}
+    public static Actor stringToActor(String input){
+        Actor actor;
+        try {
+            actor = om.readValue(input, Actor.class);
+        } catch (JsonProcessingException e) {
+            System.err.println("JsonProcessingException in method: stringToActor");
+            return null;
+        }
+        return actor;
+    }
 
 }
